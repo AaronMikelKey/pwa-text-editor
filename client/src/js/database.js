ecc0d8212a5cee030389a29d6593385e6e0e755f
Aaron Key <aaronmikelkey@gmail.com>
2022-10-20 15:07:00 -0400

    Add IndexDB functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,19 +7,38 @@ const initdb = async () =>
 				console.log('jate database already exists')
 				return
 			}
-			db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true })
-			console.log('jate database created')
+			return (
+				db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true }),
+				console.log('jate database created')
+			)
 		},
 	})
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-	return (await initdb()).add(content)
+	console.log('POST to the DB')
+	const jateDb = await openDB('jate', 1)
+
+	const tx = jateDb.transaction('jate', 'readwrite')
+	const store = tx.objectStore('jate')
+	const request = store.add({ content: content })
+	const result = await request
+	console.log('Data saved to DB: ', result)
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-	return (await initdb()).getAll()
+	const jateDb = await openDB('jate', 1)
+
+	const tx = jateDb.transaction('jate', 'readonly')
+
+	const store = tx.objectStore('jate')
+
+	const request = store.getAll()
+
+	const result = await request
+	console.log('result.value = ', result.pop().content)
+	return result.pop().content
 }
 
 initdb()
